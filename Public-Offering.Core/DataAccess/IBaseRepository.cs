8a5b1dbdd39f087437b5f0dc026192e8c4c08dbd
mehmet demircan <mehmeddemircan@gmail.com>
2023-09-07 21:37:30 +0300

    created IBaseRepository

---
+++ Public-Offering.Core/DataAccess/IBaseRepository.cs
@@ -0,0 +1,30 @@
+﻿using Public_Offering.Core.Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Public_Offering.Core.DataAccess
+{
+    public interface IBaseRepository<TEntity> where TEntity : class, IEntity, new()
+    {
+
+        Task<IEnumerable<TEntity>> GetListAsync(Expression<Func<TEntity, bool>> filter = null);
+
+        Task<TEntity> GetAsync(Expression<Func<TEntity, bool>> filter);
+
+        Task<TEntity> AddAsync(TEntity entity);
+
+        Task<TEntity> UpdateAsync(TEntity entity);
+
+        Task<bool> DeleteAsync(int id);
+
+
+        Task<IEnumerable<TEntity>> GetListAsyncPagination(int pageNumber, int pageSize, Expression<Func<TEntity, bool>> filter = null);
+
+        TEntity Get(Expression<Func<TEntity, bool>> filter);
+
+    }
+}
