51bd1b8f20232da1f023a69ecca1089a78e8aebf
mehmet demircan <mehmeddemircan@gmail.com>
2023-09-07 21:41:11 +0300

    created admin companies and public offer controller

---
+++ PublicOffering.WebApi/Controllers/Areas/Admin/CompaniesController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Public_Offering.Business.Abstract;
+
+namespace PublicOffering.WebApi.Controllers.Areas.Admin
+{
+    [Route("api/[Area]/[controller]")]
+    [ApiController]
+    public class CompaniesController : ControllerBase
+    {
+        ICompanyService _companyService;
+         
+        public CompaniesController(ICompanyService companyService)
+        {
+            _companyService = companyService;
+        }
+    }
+}
---
+++ PublicOffering.WebApi/Controllers/Areas/Admin/PublicOffersController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Public_Offering.Business.Abstract;
+
+namespace PublicOffering.WebApi.Controllers.Areas.Admin
+{
+    [Route("api/[Area]7[controller]")]
+    [ApiController]
+    public class PublicOffersController : ControllerBase
+    {
+        IPublicOfferService _publicOfferService;
+
+        public PublicOffersController(IPublicOfferService publicOfferService)
+        {
+            _publicOfferService = publicOfferService;
+        }
+    }
+}
