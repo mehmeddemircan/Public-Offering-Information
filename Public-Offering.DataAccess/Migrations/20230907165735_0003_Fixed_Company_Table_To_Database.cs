cfa38d04a98b899eb327e3205aa4ad9302fdf0b0
mehmet demircan <mehmeddemircan@gmail.com>
2023-09-07 21:40:08 +0300

    created migrations

---
+++ Public-Offering.DataAccess/Migrations/20230907165735_0003_Fixed_Company_Table_To_Database.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Public_Offering.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class _0003_Fixed_Company_Table_To_Database : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Companies",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,2)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Description",
+                table: "Companies",
+                type: "decimal(18,2)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+    }
+}
