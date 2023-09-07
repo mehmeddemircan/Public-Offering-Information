529b8f6c8b69acc9f0c1171c8bf6831cb7f42ed4
mehmet demircan <mehmeddemircan@gmail.com>
2023-09-07 21:40:54 +0300

    created public offer status enum

---
+++ Public-Offering.Types/Enums/PublicOfferStatus.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Public_Offering.Types.Enums
+{
+    public enum PublicOfferStatus
+    {
+        New = 1,
+        Cancelled = 2,
+        Approved = 3,
+        NotPublicOfferNow = 4,
+
+    }
+}
