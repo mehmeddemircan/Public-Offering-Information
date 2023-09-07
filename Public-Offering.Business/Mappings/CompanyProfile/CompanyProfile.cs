6cd65ad0e3990632271c8c7c9f0ed1404ad6f40e
mehmet demircan <mehmeddemircan@gmail.com>
2023-09-07 21:36:56 +0300

    created company profile

---
+++ Public-Offering.Business/Mappings/CompanyProfile/CompanyProfile.cs
@@ -0,0 +1,31 @@
+﻿using AutoMapper;
+using Public_Offering.Types.Concrete;
+using Public_Offering.Types.DTOs.CompanyDtos;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Public_Offering.Business.Mappings.CompanyProfile
+{
+    public class CompanyProfile  : Profile
+    {
+
+        public CompanyProfile()
+        {
+            CreateMap<CompanyAddDto, Company>();
+            CreateMap<Company, CompanyAddDto>();
+
+            CreateMap<CompanyUpdateDto, Company>();
+            CreateMap<Company, CompanyUpdateDto>();
+
+            CreateMap<CompaniesDto, Company>();
+            CreateMap<Company, CompaniesDto>();
+
+            CreateMap<CompanyDetailDto, Company>();
+            CreateMap<Company, CompanyDetailDto>();
+        }
+    }
+}
