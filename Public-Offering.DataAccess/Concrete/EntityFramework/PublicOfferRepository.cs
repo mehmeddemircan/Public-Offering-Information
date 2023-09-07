d9a19fa4f36db96649921401008946063a5a3680
mehmet demircan <mehmeddemircan@gmail.com>
2023-09-07 21:39:47 +0300

    created public offer repository

---
+++ Public-Offering.DataAccess/Concrete/EntityFramework/PublicOfferRepository.cs
@@ -0,0 +1,16 @@
+﻿using Public_Offering.Core.DataAccess.EntityFramework;
+using Public_Offering.DataAccess.Abstract;
+using Public_Offering.DataAccess.Contexts;
+using Public_Offering.Types.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Public_Offering.DataAccess.Concrete.EntityFramework
+{
+    public class PublicOfferRepository : EfBaseRepository<PublicOffer,ApplicationDbContext> , IPublicOfferRepository
+    {
+    }
+}
