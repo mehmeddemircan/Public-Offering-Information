30afce1bb8a5f449b9809465fd49d46e3c62b664
mehmet demircan <mehmeddemircan@gmail.com>
2023-09-07 21:35:55 +0300

    created public offer service

---
+++ Public-Offering.Business/Abstract/IPublicOfferService.cs
@@ -0,0 +1,27 @@
+﻿using Public_Offering.Core.Utilities.Results;
+using Public_Offering.Types.Concrete;
+using Public_Offering.Types.DTOs.PublicOfferDtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Public_Offering.Business.Abstract
+{
+    public interface IPublicOfferService 
+    {
+        Task<IResult> AddAsync(PublicOfferAddDto entity);
+
+
+        Task<IDataResult<IEnumerable<PublicOffersDto>>> GetListAsync(Expression<Func<PublicOffer, bool>> filter = null);
+        Task<IDataResult<PublicOfferDetailDto>> GetAsync(Expression<Func<PublicOffer, bool>> filter);
+
+        Task<IDataResult<PublicOfferDetailDto>> GetByIdAsync(int id);
+
+        Task<IDataResult<PublicOfferUpdateDto>> UpdateAsync(PublicOfferUpdateDto PublicOfferUpdateDto);
+
+        Task<IDataResult<bool>> DeleteAsync(int id);
+    }
+}
